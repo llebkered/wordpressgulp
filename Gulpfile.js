028794948e2c45565e5b4acb400f5f2818c08325
Derek Bell <luicd@lucid.com.au>
2017-02-20 16:28:40 +1100

    first commit

---
+++ Gulpfile.js
@@ -0,0 +1,140 @@
+// Gulp & utilities
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var watch = require('gulp-watch');
+//var browserSync = require('browser-sync').create();
+
+// CSS
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var csso = require('gulp-csso');
+
+// Image
+/*
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
+var svgstore = require('gulp-svgstore');
+var svgmin = require('gulp-svgmin');
+var path = require('path');
+var svgfallback = require('gulp-svgfallback');
+*/
+// Javascript
+
+
+
+
+/*
+CSS
+Compile sass
+add browser prefix
+minify
+
+Images
+minify images
+combine svg
+make png backup files
+make sprite sheet
+
+JS
+concat JS
+minify JS
+
+web
+Live reload
+
+*/
+
+/* ================= */
+/* CSS Files */
+
+// Compile SASS and Autoprefix.
+gulp.task('sass', function() {
+  gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sass({
+      outputStyle: 'expanded'
+    }))
+    .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {
+      cascade: true
+    }))
+    // Minify CSS
+  //  .pipe(csso())
+    .pipe(gulp.dest('./'));
+    //.pipe(browserSync.stream());
+});
+
+
+/* ================= */
+/* Images */
+
+// Compress images
+/*
+gulp.task('images', function() {
+  return gulp.src('assets/images/*')
+    .pipe(imagemin({
+      progressive: true,
+      svgoPlugins: [{
+        removeViewBox: false
+      }],
+      use: [pngquant()]
+    }))
+    .pipe(gulp.dest('assets/build/images'));
+});
+*/
+// Combine SVGs into 1 file
+/*
+gulp.task('svgstore', function () {
+    return gulp
+        .src('assets/svgs/*.svg')
+        .pipe(svgmin(function (file) {
+            var prefix = path.basename(file.relative, path.extname(file.relative));
+            return {
+                plugins: [{
+                    cleanupIDs: {
+                        prefix: prefix + '-',
+                        minify: true
+                    }
+                }]
+            };
+        }))
+        .pipe(svgstore({ inlineSvg: true }))
+        .pipe(gulp.dest('assets/build/svg'));
+});
+
+// Create PNG sprite sheet fallback
+gulp.task('svgfallback', function () {
+    return gulp
+        .src('assets/svgs/*.svg', {base: 'assets/build/svg'})
+        .pipe(svgfallback())
+        .pipe(gulp.dest('assets/build/images/png'));
+});
+*/
+
+/* ================= */
+/* browser-sync */
+/*
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        proxy: "my-local-site.dev"
+    });
+});
+*/
+/* ================= */
+/* Gulp Watch */
+
+gulp.task('watch', function() {
+  // watch scss files
+  gulp.watch('assets/sass/**/*.scss', ['sass']);
+  // watch image files
+  //gulp.watch('assets/images/**/*', ['images']);
+
+});
+
+
+/*gulp.task('default', function() {
+  // place code for your default task here
+});
+*/
+//gulp.task('default', ['sass', 'images' , 'svgstore', 'svgfallback', 'browser-sync','watch']);
+
+gulp.task('default', ['sass', 'watch']);
---
+++ readme.md
@@ -0,0 +1,21 @@
+# Wordpress Theme Gulp Starter
+
+v 1.0
+
+20/2/2017
+
+A very simple starter package for Gulp and a blank Wordpress theme. I usually use underscores as my Starter theme.
+
+This version only compiles SASS into CSS.
+
+There are other options in the Gulpfile to combine and minify SVG's but these have been commented out.
+
+* [underscores.me](underscores.me)
+
+## Instructions
+
+1. Copy Gulpfile.js and package.json to the root of your theme.
+
+2. Adjust Gulpfile  and package.json to suit your needs
+
+3. Run *npm install* to install the node packages.
